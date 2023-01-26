2e2bd496253dcde4a697ab2658d8dd2653601929
Igor Agapie <igoragapie@gmail.com>
2023-01-27 00:03:06 +0100

    fix redirect

--- render/redirect.go
+++ render/redirect.go
@@ -15,8 +15,11 @@ func (r Redirect) Render(w http.ResponseWriter) error {
 	if (r.Code < http.StatusMultipleChoices || r.Code > http.StatusPermanentRedirect) && r.Code != http.StatusCreated {
 		panic(fmt.Sprintf("Cannot redirect with status code %d", r.Code))
 	}
+
 	http.Redirect(w, r.Request, r.Location, r.Code)
-	return nil
+
+	_, err := w.Write(nil)
+	return err
 }
 
 func (r Redirect) WriteContentType(http.ResponseWriter) {}
